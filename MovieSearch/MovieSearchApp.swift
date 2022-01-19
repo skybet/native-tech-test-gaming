7dda45068caa4e90547844c3795d20189ed3c8bd
Ananth Jk <ananth.jayabalkalpanadevi@natwest.com>
2022-01-19 21:45:21 +0000

    Initial Commit

---
+++ MovieSearch/MovieSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchApp.swift
+//  MovieSearch
+//
+//  Created by Ananth Jk on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
